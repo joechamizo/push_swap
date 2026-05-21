77f37f5661f228a3df914eb39ec25e2ddf68519d
Joaquin Martinez Chamizo <joaqumar@car7s3.42barcelona.com>
2026-05-21 09:10:10 +0200

    [mod] Project push_swap

--- srcs/benchmark.c
+++ srcs/benchmark.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: acoromin <acoromin@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/20 00:00:00 by acoromin          #+#    #+#             */
-/*   Updated: 2026/05/20 00:00:00 by acoromin         ###   ########.fr       */
+/*   Created: 2026/05/21 09:00:00 by acoromin          #+#    #+#             */
+/*   Updated: 2026/05/21 09:10:00 by acoromin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,6 @@ double	calculate_disorder(t_stack *stack, int size)
 	t_stack	*i;
 	t_stack	*j;
 	int		inversions;
-	int		total_pairs;
 
 	if (size <= 1)
 		return (0.0);
@@ -62,39 +61,5 @@ double	calculate_disorder(t_stack *stack, int size)
 		}
 		i = i->next;
 	}
-	total_pairs = (size * (size - 1)) / 2;
-	return (((double)inversions / total_pairs) * 100.0);
-}
-
-static void	print_op_count(t_program *prog, char *name, int index)
-{
-	if (prog->op_counts[index] > 0)
-	{
-		ft_putstr_fd(name, 2);
-		ft_putstr_fd(": ", 2);
-		ft_putnbr_fd(prog->op_counts[index], 2);
-		ft_putstr_fd("\n", 2);
-	}
-}
-
-void	print_benchmark(t_program *prog)
-{
-	ft_putstr_fd("\n--- PUSH_SWAP BENCHMARK ---\n", 2);
-	ft_putstr_fd("operations: ", 2);
-	ft_putnbr_fd(prog->total_ops, 2);
-	ft_putstr_fd("\ndisorder: ", 2);
-	print_disorder(prog->disorder_index);
-	ft_putstr_fd("strategy: ", 2);
-	print_strategy_info(prog);
-	print_op_count(prog, "sa", 0);
-	print_op_count(prog, "sb", 1);
-	print_op_count(prog, "ss", 2);
-	print_op_count(prog, "pa", 3);
-	print_op_count(prog, "pb", 4);
-	print_op_count(prog, "ra", 5);
-	print_op_count(prog, "rb", 6);
-	print_op_count(prog, "rr", 7);
-	print_op_count(prog, "rra", 8);
-	print_op_count(prog, "rrb", 9);
-	print_op_count(prog, "rrr", 10);
+	return (((double)inversions / ((size * (size - 1)) / 2)) * 100.0);
 }
--- srcs/benchmark_utils.c
+++ srcs/benchmark_utils.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: acoromin <acoromin@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/20 00:00:00 by acoromin          #+#    #+#             */
-/*   Updated: 2026/05/20 00:00:00 by acoromin         ###   ########.fr       */
+/*   Created: 2026/05/21 09:15:00 by acoromin          #+#    #+#             */
+/*   Updated: 2026/05/21 09:25:00 by acoromin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	print_disorder(double value)
 
 	scaled = (int)(value * 100.0 + 0.5);
 	decimal = scaled % 100;
+	ft_putstr_fd("[bench] disorder: ", 2);
 	ft_putnbr_fd(scaled / 100, 2);
 	ft_putstr_fd(".", 2);
 	if (decimal < 10)
@@ -29,16 +30,55 @@ void	print_disorder(double value)
 
 void	print_strategy_info(t_program *prog)
 {
+	ft_putstr_fd("[bench] strategy: ", 2);
 	if (prog->strategy == STRAT_SIMPLE)
 		ft_putstr_fd("Simple / O(n^2)\n", 2);
 	else if (prog->strategy == STRAT_MEDIUM)
-		ft_putstr_fd("Medium / O(n sqrt(n))\n", 2);
+		ft_putstr_fd("Medium / O(n \xE2\x88\x9An)\n", 2);
 	else if (prog->strategy == STRAT_COMPLEX)
 		ft_putstr_fd("Complex / O(n log n)\n", 2);
 	else if (prog->disorder_index < 20.0)
 		ft_putstr_fd("Adaptive / O(n)\n", 2);
 	else if (prog->disorder_index < 50.0)
-		ft_putstr_fd("Adaptive / O(n sqrt(n))\n", 2);
+		ft_putstr_fd("Adaptive / O(n\xE2\x88\x9An)\n", 2);
 	else
 		ft_putstr_fd("Adaptive / O(n log n)\n", 2);
+}
+
+static void	print_ops_line(t_program *p, int start, int end, char **names)
+{
+	ft_putstr_fd("[bench]", 2);
+	while (start <= end)
+	{
+		ft_putstr_fd(" ", 2);
+		ft_putstr_fd(names[start], 2);
+		ft_putstr_fd(": ", 2);
+		ft_putnbr_fd(p->op_counts[start], 2);
+		start++;
+	}
+	ft_putstr_fd("\n", 2);
+}
+
+void	print_benchmark(t_program *prog)
+{
+	char	*names[11];
+
+	names[0] = "sa";
+	names[1] = "sb";
+	names[2] = "ss";
+	names[3] = "pa";
+	names[4] = "pb";
+	names[5] = "ra";
+	names[6] = "rb";
+	names[7] = "rr";
+	names[8] = "rra";
+	names[9] = "rrb";
+	names[10] = "rrr";
+	print_disorder(prog->disorder_index);
+	print_strategy_info(prog);
+	ft_putstr_fd("[bench] total_ops: ", 2);
+	ft_putnbr_fd(prog->total_ops, 2);
+	ft_putstr_fd("\n", 2);
+	print_ops_line(prog, 0, 4, names);
+	print_ops_line(prog, 5, 10, names);
 }
